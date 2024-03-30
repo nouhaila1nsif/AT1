b5add7f23f6220cd6ff04e8ba78987d20d858b38
nouha <nouha@NOUHAILA>
2024-03-30 23:58:04 +0000

    2eme class

--- atelier/Class2.cs
+++ atelier/Class2.cs
@@ -1,56 +1,6 @@
-﻿using System;
-
-namespace Atelier
+﻿namespace atelier
 {
-    public class Personne
+    public class Class2
     {
-        // Attributs privés
-        private string nom;
-        private int age;
-        private Adresse adresse;
-
-        // Constructeur par défaut
-        public Personne()
-        {
-            nom = "Inconnu";
-            age = 0;
-            adresse = new Adresse();
-        }
-
-        // Constructeur avec paramètres
-        public Personne(string nom, int age, Adresse adresse)
-        {
-            this.nom = nom;
-            this.age = age;
-            this.adresse = adresse;
-        }
-
-        // Propriétés publiques
-        public string Nom
-        {
-            get { return nom; }
-            set { nom = value; }
-        }
-
-        public int Age
-        {
-            get { return age; }
-            set { age = value; }
-        }
-
-        public Adresse Adresse
-        {
-            get { return adresse; }
-            set { adresse = value; }
-        }
-
-        // Méthode pour afficher les informations de la personne
-        public void AfficherInformations()
-        {
-            Console.WriteLine("Nom : " + nom);
-            Console.WriteLine("Age : " + age);
-            Console.WriteLine("Adresse : ");
-            adresse.AfficherInformations();
-        }
     }
 }
