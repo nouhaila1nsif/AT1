23307f449d25ae7a79b331365e81926ee9a7b9de
nouha <nouha@NOUHAILA>
2024-03-30 23:25:14 +0000

    1er modif

--- atelier/Class1.cs
+++ atelier/Class1.cs
@@ -2,5 +2,42 @@
 {
     public class Class1
     {
+      namespace Atelier
+    {
+        public class Class1
+        {
+            // Déclaration d'une variable de type string
+            private string nom;
+
+            // Constructeur par défaut
+            public Class1()
+            {
+                // Initialisation de la variable nom
+                nom = "Inconnu";
+            }
+
+            // Méthode pour afficher le nom
+            public void AfficherNom()
+            {
+                Console.WriteLine("Le nom est : " + nom);
+            }
+
+            // Méthode pour modifier le nom
+            public void ModifierNom(string nouveauNom)
+            {
+                // Vérification de la longueur du nouveau nom
+                if (nouveauNom.Length > 20)
+                {
+                    Console.WriteLine("Le nom est trop long. Veuillez entrer un nom de 20 caractères maximum.");
+                }
+                else
+                {
+                    // Affectation du nouveau nom
+                    nom = nouveauNom;
+                }
+            }
+        }
     }
+
+}
 }
