83e62662f84ec21d3b8e4dea51fa074614f99e0f
nouha <nouha@NOUHAILA>
2024-03-31 00:09:00 +0000

    1 commit branch

--- atelier/Class3.cs
+++ atelier/Class3.cs
@@ -1,6 +1,55 @@
-﻿namespace atelier
+﻿using System;
+
+namespace Atelier
 {
-    public class Class3
+    public class Adresse
     {
+        // Attributs privés
+        private string rue;
+        private string codePostal;
+        private string ville;
+
+        // Constructeur par défaut
+        public Adresse()
+        {
+            rue = "Inconnu";
+            codePostal = "Inconnu";
+            ville = "Inconnu";
+        }
+
+        // Constructeur avec paramètres
+        public Adresse(string rue, string codePostal, string ville)
+        {
+            this.rue = rue;
+            this.codePostal = codePostal;
+            this.ville = ville;
+        }
+
+        // Propriétés publiques
+        public string Rue
+        {
+            get { return rue; }
+            set { rue = value; }
+        }
+
+        public string CodePostal
+        {
+            get { return codePostal; }
+            set { codePostal = value; }
+        }
+
+        public string Ville
+        {
+            get { return ville; }
+            set { ville = value; }
+        }
+
+        // Méthode pour afficher les informations de l'adresse
+        public void AfficherInformations()
+        {
+            Console.WriteLine("Rue : " + rue);
+            Console.WriteLine("Code postal : " + codePostal);
+            Console.WriteLine("Ville : " + ville);
+        }
     }
 }
